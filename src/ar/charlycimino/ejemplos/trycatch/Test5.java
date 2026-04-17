103fd976430ef8b8a5aaadad484c138cdf9d0385
FelixBriz7 <felixbriz077@gmail.com>
2026-04-18 00:23:01 +0200

    Fix: resolver issues de Checkstyle en Test5.java (Sun Checks)
    - HideUtilityClassConstructor: Añadido constructor privado y declarada la clase como final
    - MissingJavadocMethod: Añadido Javadoc al método main()
    - FinalParameters: Declarado args como final en main()
    - MagicNumber: Extraído el valor 2147483647 a la constante MAX_INT

--- src/ar/charlycimino/ejemplos/trycatch/Test5.java
+++ src/ar/charlycimino/ejemplos/trycatch/Test5.java
@@ -1,4 +1,3 @@
-
 package ar.charlycimino.ejemplos.trycatch;
 
 /**
@@ -7,13 +6,23 @@ package ar.charlycimino.ejemplos.trycatch;
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
-public class Test5 {
+public final class Test5 {
+
+    /** Valor máximo de un entero. */
+    private static final int MAX_INT = 2147483647;
+
+    private Test5() {
+    }
 
     /*
       ¿Dónde se lanza la excepción?
     */
-    public static void main(String[] args) {
-        int x = Math.incrementExact(2147483647);
+    /**
+     * Prueba dónde se lanza la excepción.
+     * @param args argumentos de línea de comandos
+     */
+    public static void main(final String[] args) {
+        int x = Math.incrementExact(MAX_INT);
         System.out.println(x);
     }
 
